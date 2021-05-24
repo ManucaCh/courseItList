3bcf35ff001d1a5489ab0a2f6b22a9fe618e5867
ManucaCh <manuchaves@gmail.com>
2021-05-24 17:47:17 -0300

    add delete on click js

--- js/app.js
+++ js/app.js
@@ -1,30 +1,44 @@
+let idList = 0;
+
 function clearInput() {
   document.getElementById("itemInput").value = "";
-}
-
-function addItem() {
-  const inputItem = document.getElementById("itemInput");
-  console.log(inputItem.value);
-  clearInput();
 }
 
 function enterItem(e) {
   const list = document.getElementById("list");
   const oldHTML = list.innerHTML;
+  const itemValue = document.getElementById("itemInput").value;
+
   if (e.keyCode === 13) {
-    list.innerHTML = `${oldHTML} <li>${
-      document.getElementById("itemInput").value
-    }</li>`;
-    clearInput();
+    if (itemValue === "") {
+      alert("No podes agregar un elemento vacio!");
+    } else {
+      list.innerHTML = `${oldHTML} <li onclick="remove(${idList})" id="${idList}">${
+        document.getElementById("itemInput").value
+      }</li>`;
+      idList++;
+      clearInput();
+    }
   }
 }
 
 function addItemHTML() {
   const list = document.getElementById("list");
   const oldHTML = list.innerHTML;
+  const itemValue = document.getElementById("itemInput").value;
 
-  list.innerHTML = `${oldHTML} <li>${
-    document.getElementById("itemInput").value
-  }</li>`;
-  clearInput();
+  if (itemValue === "") {
+    alert("No podes agregar un elemento vacio!");
+  } else {
+    list.innerHTML = `${oldHTML} <li onclick="remove(${idList})" id="${idList}">${
+      document.getElementById("itemInput").value
+    }</li>`;
+    idList++;
+    clearInput();
+  }
+}
+
+function remove(idList) {
+  const liItem = document.getElementById(idList);
+  liItem.remove();
 }
